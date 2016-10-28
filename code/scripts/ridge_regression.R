e0ef601463880e408156d125d8f8c493ff460a25
Joseph Francia <josephfrancia@Josephs-MacBook-Pro.local>
2016-10-28 14:46:28 -0700

    additional modeling for ridge

--- code/scripts/ridge_regression.R
+++ code/scripts/ridge_regression.R
@@ -1,6 +1,20 @@
-setwd("/Users/josephfrancia/Desktop/Fall_2016/Stats159/stat159-fall2016-project2/code/scripts")
+#setwd("/Users/josephfrancia/Desktop/Fall_2016/Stats159/stat159-fall2016-project2/code/scripts")
 load(file="../../data/test_train.RData")
-ridge_obj=glmnet(as.matrix(train[,-c(1,2,14)]), train[,14])
+library(glmnet)
+grid=10^seq(10,-2, length=100)
+ridge_obj=cv.glmnet(as.matrix(train[,-c(1,2,14)]), train[,14], lambda=grid, intercept=FALSE, standardize=FALSE)
+save(file="../../data/ridge_fit.RData")
+opt_lambda=ridge_obj$lambda.1se
+plot(ridge_obj)
+
+
+
+
+
+
+
+
+
 avg_error=vector()
 for(i in 1:length(ridge_obj$lambda)){
   y_hat=as.matrix(cbind(test[-c(1,2,14)])) %*% ridge_obj$beta[,i]
@@ -12,3 +26,4 @@ opt_lambda=ridge_obj$lambda[opt_indices]
 
 y_hat=as.matrix(cbind(test[-c(1,2,14)])) %*% ridge_obj$beta[,opt_indices]
 plot(y_hat-test[,14])
+mean(y_hat-test[,14])
