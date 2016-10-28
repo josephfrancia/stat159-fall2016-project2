d572fb905a436d977cb6225b54c9418298725211
Joseph Francia <josephfrancia@Josephs-MacBook-Pro.local>
2016-10-28 15:14:53 -0700

    finished ridge model building

--- code/scripts/ridge_regression.R
+++ code/scripts/ridge_regression.R
@@ -1,34 +1,39 @@
 #setwd("/Users/josephfrancia/Desktop/Fall_2016/Stats159/stat159-fall2016-project2/code/scripts")
+
+#Finding various lambda values and their associated MSE
+set.seed(0)
 load(file="../../data/test_train.RData")
 library(glmnet)
 grid=10^seq(10,-2, length=100)
 ridge_obj=cv.glmnet(as.matrix(train[,-c(1,2,14)]), train[,14], lambda=grid, intercept=FALSE, standardize=FALSE)
-save(file="../../data/ridge_fit.RData")
 
 #Ridge Plot
 png(filename="../../images/plot-Ridge.png")
 plot(ridge_obj)
 dev.off()
 
-#Computing MSE of Ridge With Optimal Lambda
+#Computing MSE of Ridge with optimal lambda
 opt_ridge=glmnet(as.matrix(train[,-c(1,2,14)]), train[,14], lambda=ridge_obj$lambda.1se, intercept=FALSE, standardize=FALSE)
-y_hat=as.matrix(cbind(test[-c(1,2,14)])) %*% opt_ridge$beta
+y_hat=as.matrix(test[,-c(1,2,14)]) %*% opt_ridge$beta
 mse_ridge=mean(y_hat - test[,14])
 
 
+#Finding beta coefficients by fitting model on full dataset
+full_ridge=glmnet(as.matrix(rbind(train[,-c(1,2,14)], test[,-c(1,2,14)])), c(train[,14], test[,14]), lambda=ridge_obj$lambda.1se, intercept=FALSE, standardize=FALSE)
+
+#Saving all relevant ridge objects
+save(ridge_obj, mse_ridge, full_ridge$beta, ridge_obj$lambda.1se, filename="../../data/ridge.RData")
 
 
+sink(file="../../data/ridge.txt")
+print("Optimal Lambda for Ridge")
+ridge_obj$lambda.1se
+
+print("MSE of Ridge with Optimal Lambda")
+mse_ridge
+
+print("Beta Coefficients")
+full_ridge$beta
+sink()
 
 
-avg_error=vector()
-for(i in 1:length(ridge_obj$lambda)){
-  y_hat=as.matrix(cbind(test[-c(1,2,14)])) %*% ridge_obj$beta[,i]
-  avg_error[i]=mean(y_hat - test[,14])
-}
-
-opt_indices=which(abs(avg_error)==min(abs(avg_error)))
-opt_lambda=ridge_obj$lambda[opt_indices]
-
-y_hat=as.matrix(cbind(test[-c(1,2,14)])) %*% ridge_obj$beta[,opt_indices]
-plot(y_hat-test[,14])
-mean(y_hat-test[,14])
