482b090b3598350e40b9ccf23ecc45f371eae25d
Joseph Francia <josephfrancia@Josephs-MacBook-Pro.local>
2016-11-04 10:43:34 -0700

    fixed compare file

--- code/scripts/comparing_regressions.R
+++ code/scripts/comparing_regressions.R
@@ -20,9 +20,10 @@ betas_ls=as.numeric(lm_obj$coefficients)[2:12]
 betas_pcr=as.data.frame(pcr_coefficients)
 betas_pcr=betas_pcr[,pcr_lowest_comp]
 betas_lasso=betas_lasso
-betas=data.frame(betas_plsr[-1], betas_ls, betas_ridge,betas_pcr,betas_lasso)
+betas=data.frame(betas_plsr, betas_ls, betas_ridge,betas_pcr,betas_lasso)
 colnames(betas)=c("Coefficients for PLS Regression", "Coefficients for Least Squares", "Coefficients for Ridge Regression","Coefficients for PCR","Coefficients for Lasso Regression")
-
+rownames(betas)=c("Income", "Limit", "Rating", "Cards", "Age", "Education", "GenderFemale", "StudentYes","MarriedYes", "EthnicityAsian", "Ethnicity Caucasian")
+save(betas, file="data/betas.RData")
 
 #png("images/beta.png")
 #grid.table(betas)
@@ -38,17 +39,17 @@ dev.off()
 
 #Coefficient Plot
 png(filename="images/coefficient_plot.png")
-plot(seq(1,5, by=1),as.numeric(betas[1,2:6]), xaxt="n", type="l", col="deeppink",xlab="Regression Type", ylab="Coefficient Value", ylim=c(-3,1), main="Coefficient Values For Each Regression")
-lines(seq(1,5, by=1),as.numeric(betas[2,2:6]), col="blue")
-lines(seq(1,5, by=1),as.numeric(betas[3,2:6]), col="red")
-lines(seq(1,5, by=1),as.numeric(betas[4,2:6]), col="yellow")
-lines(seq(1,5, by=1),as.numeric(betas[5,2:6]), col="green")
-lines(seq(1,5, by=1),as.numeric(betas[6,2:6]), col="orange")
-lines(seq(1,5, by=1),as.numeric(betas[7,2:6]), col="purple")
-lines(seq(1,5, by=1),as.numeric(betas[8,2:6]), col="black")
-lines(seq(1,5, by=1),as.numeric(betas[9,2:6]), col="gray")
-lines(seq(1,5, by=1),as.numeric(betas[10,2:6]), col="brown")
-lines(seq(1,5, by=1),as.numeric(betas[11,2:6]), col="tomato")
+plot(seq(1,5, by=1),as.numeric(betas[1,1:5]), xaxt="n", type="l", col="deeppink",xlab="Regression Type", ylab="Coefficient Value", ylim=c(-3,1), main="Coefficient Values For Each Regression")
+lines(seq(1,5, by=1),as.numeric(betas[2,1:5]), col="blue")
+lines(seq(1,5, by=1),as.numeric(betas[3,1:5]), col="red")
+lines(seq(1,5, by=1),as.numeric(betas[4,1:5]), col="yellow")
+lines(seq(1,5, by=1),as.numeric(betas[5,1:5]), col="green")
+lines(seq(1,5, by=1),as.numeric(betas[6,1:5]), col="orange")
+lines(seq(1,5, by=1),as.numeric(betas[7,1:5]), col="purple")
+lines(seq(1,5, by=1),as.numeric(betas[8,1:5]), col="black")
+lines(seq(1,5, by=1),as.numeric(betas[9,1:5]), col="gray")
+lines(seq(1,5, by=1),as.numeric(betas[10,1:5]), col="brown")
+lines(seq(1,5, by=1),as.numeric(betas[11,1:5]), col="tomato")
 axis(side=1,labels=c("PLS", "OLS", "Ridge","PCR","Lasso"), at=c(1,2,3,4,5))
 legend(1.5,-.7, bty="n", c("Income","Limit", "Rating", "Cards", "Age", "Education", "Gender.Male",
                 "GenderFemale","MarriedYes", "EthnicityAsian","EthnicityCaucasian"),
